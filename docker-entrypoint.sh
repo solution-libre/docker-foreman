b1b42884f6fb398af0b6fb60268da8ab452a0c49
Florent Poinsaut <florent@poinsaut.fr>
2018-04-26 17:05:22 +0200

    add db configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+        local var="$1"
+        local fileVar="${var}_FILE"
+        local def="${2:-}"
+        if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+                echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+                exit 1
+        fi
+        local val="$def"
+        if [ "${!var:-}" ]; then
+                val="${!var}"
+        elif [ "${!fileVar:-}" ]; then
+                val="$(< "${!fileVar}")"
+        fi
+        export "$var"="$val"
+        unset "$fileVar"
+}
 
 if [ ! "$(ls -A /etc/foreman)" -o ! "$(ls -A /etc/foreman-proxy)" ]; then
 	apt-get update
+	file_env 'FOREMAN_DB_PASSWORD' 'foreman_password'
+	sed -i -e "s/^  db_password:.*/  db_password: ${FOREMAN_DB_PASSWORD}/" /etc/foreman-installer/scenarios.d/foreman-answers.yaml
 	foreman-installer
 	rm -rf /var/lib/apt/lists/*
 fi
